6998642e1362a3aaff59617016659c329fca1e63
Evan Thomas <evanthomas0292@gmail.com>
2023-04-12 22:39:31 +0100

    Refactor DiaryEntry with attr_reader

--- skill_challenges/lib/diary_entry.rb
+++ skill_challenges/lib/diary_entry.rb
@@ -1,15 +1,9 @@
 class DiaryEntry
+  attr_reader :title, :contents
+  
   def initialize(title, contents)
     @title = title
     @contents = contents
-  end
-
-  def title
-    @title
-  end
-
-  def contents
-    @contents
   end
 
   def count_words
