0e310404f6d61937a6b977f45303ef0a70b35327
Evan Thomas <evanthomas0292@gmail.com>
2023-04-12 10:42:35 +0100

    Add count _words method and tests

--- lib/diary.rb
+++ lib/diary.rb
@@ -3,3 +3,8 @@ def make_snippet(str)
   first_five_words = words.slice(0, 5).join(" ")
   words.length <= 5 ? first_five_words : first_five_words + "..."
 end
+
+def count_words(str)
+  words = str.split(" ")
+  words.length
+end
--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -18,5 +18,21 @@ describe "make_snippet method" do
     result = make_snippet(str)
     expect(result).to eq "This is a test!"
   end
+
+  it "returns empty string when given empty string" do
+    result = make_snippet("")
+    expect(result).to eq ""
+  end
 end
 
+describe "count_words method" do
+  it "takes a string as argument and returns the number of words in the string" do
+    result = count_words("This is a test")
+    expect(result).to eq 4
+  end
+
+  it "returns 0 when given empty string" do
+    result = count_words("")
+    expect(result).to eq 0
+  end
+end
