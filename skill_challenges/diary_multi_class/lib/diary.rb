7fe317fb040cf2b2f804491e2e6fee58fd8c5a5e
Evan Thomas <evanthomas0292@gmail.com>
2023-04-14 18:59:46 +0100

    Add more integration tests for find_best_entry_for_reading_time method and implement.

--- skill_challenges/diary_multi_class/lib/diary.rb
+++ skill_challenges/diary_multi_class/lib/diary.rb
@@ -4,8 +4,7 @@ class Diary
     @entries = []
   end
 
-  def add(entry) # entry is an instance of DiaryEntry
-    # Returns nothing
+  def add(entry)
     @entries << entry
   end
 
@@ -17,26 +16,18 @@ class Diary
     @entries.sum(&:count_words)
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # if the user were to read all entries in the diary.
+  def reading_time(wpm)
     words = count_words
     (words / wpm.to_f).ceil
   end
 
   def find_best_entry_for_reading_time(wpm, minutes)
-        # `wpm` is an integer representing the number of words the user can read
-        # per minute.
-        # `minutes` is an integer representing the number of minutes the user
-        # has to read.
-    # Returns an instance of diary entry representing the entry that is closest 
-    # to, but not over, the length that the user could read in the minutes they
-    # have available given their reading speed.
     fail "There are no entries" if @entries.empty?
-    
+
     max_words = wpm * minutes
     possible_reads = @entries.select { |entry| entry.count_words <= max_words }
-    possible_reads.max(&:count_words)
+    fail "All the entries are too long to read" if possible_reads.empty?
+    
+    possible_reads.max {|entry| entry.count_words }
   end
 end
--- skill_challenges/diary_multi_class/spec/integration_spec.rb
+++ skill_challenges/diary_multi_class/spec/integration_spec.rb
@@ -2,38 +2,38 @@ require 'diary'
 require 'diary_entry'
 
 describe "Diary and DiaryEntry integration" do
-  context "when two DiaryEntry objects are added to Diary" do
-    it "returns a list of those instances of DiaryEntry" do
-      diary = Diary.new
-      diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
-      diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
-      diary.add(diary_entry1)
-      diary.add(diary_entry2)
-      result = diary.all
-      expect(result).to eq [diary_entry1, diary_entry2]
-    end
-    
-    it "counts the total number of words in all diary entries" do
-      diary = Diary.new
-      diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
-      diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
-      diary.add(diary_entry1)
-      diary.add(diary_entry2)
-      result = diary.count_words
-      expect(result).to eq 13
-    end
-    
-    it "returns an estimate of the reading time for all entries" do
-      diary = Diary.new
-      diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
-      diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
-      diary.add(diary_entry1)
-      diary.add(diary_entry2)
-      result = diary.reading_time(2)
-      expect(result).to eq 7
-    end
-    
-    it "finds the best entry to read given wpm and minutes available for reading" do
+  it "returns a list of diary entries" do
+    diary = Diary.new
+    diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
+    diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
+    diary.add(diary_entry1)
+    diary.add(diary_entry2)
+    result = diary.all
+    expect(result).to eq [diary_entry1, diary_entry2]
+  end
+  
+  it "counts the total number of words in all diary entries" do
+    diary = Diary.new
+    diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
+    diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
+    diary.add(diary_entry1)
+    diary.add(diary_entry2)
+    result = diary.count_words
+    expect(result).to eq 13
+  end
+  
+  it "returns an estimate of the reading time for all entries" do
+    diary = Diary.new
+    diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
+    diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
+    diary.add(diary_entry1)
+    diary.add(diary_entry2)
+    result = diary.reading_time(2)
+    expect(result).to eq 7
+  end
+  
+  describe "#find_best_entry_for_reading_time" do
+    it "doesn't return an entry that's too long" do
       diary = Diary.new
       diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
       diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
@@ -41,6 +41,27 @@ describe "Diary and DiaryEntry integration" do
       diary.add(diary_entry2)
       result = diary.find_best_entry_for_reading_time(2, 3)
       expect(result).to eq diary_entry1
+    end
+    
+    it "finds the longest entry that the user can read in the time" do
+      diary = Diary.new
+      diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
+      diary_entry2 = DiaryEntry.new("title2", "this is the contents for the second diary entry.")
+      diary_entry3 = DiaryEntry.new("title3", "this is the contents for the third diary entry that is too long to read.")
+      diary.add(diary_entry1)
+      diary.add(diary_entry2)
+      diary.add(diary_entry3)
+      result = diary.find_best_entry_for_reading_time(2, 5)
+      expect(result).to eq diary_entry2
+    end
+    
+    it "fails when all the entries are too long to read" do
+      diary = Diary.new
+      diary_entry1 = DiaryEntry.new("title1", "diary entry 1 contents.")
+      diary.add(diary_entry1)
+      expect {
+        diary.find_best_entry_for_reading_time(1, 1)
+      }.to raise_error "All the entries are too long to read"
     end
   end
 end
