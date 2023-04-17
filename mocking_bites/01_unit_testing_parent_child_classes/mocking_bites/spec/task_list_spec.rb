3edfb00f5881fb7093537714ec628592eb45fb33
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-04-17 13:30:15 +0100

    mocking_bites TaskList all method testing complete

--- mocking_bites/01_unit_testing_parent_child_classes/mocking_bites/spec/task_list_spec.rb
+++ mocking_bites/01_unit_testing_parent_child_classes/mocking_bites/spec/task_list_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  context "all method testing" do
+    it 'returns correctly for single task' do
+      task_list = TaskList.new
+      task_dbl = double :Task
+      task_list.add(task_dbl)
+      expect(
+        task_list.all
+      ).to eq [task_dbl]
+    end
+    it 'returns correctly for multiple tasks' do
+      task_list = TaskList.new
+      task_1_dbl = double :Task
+      task_2_dbl = double :Task
+      task_list.add(task_1_dbl)
+      task_list.add(task_2_dbl)
+      expect(
+        task_list.all
+      ).to eq [task_1_dbl, task_2_dbl]
+    end
+  end
 end
