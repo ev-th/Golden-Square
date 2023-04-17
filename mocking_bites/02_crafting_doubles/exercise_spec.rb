62e07b3aae2c5c396b9a599a42bcedd5857c3571
Evan Thomas <evanthomas0292@gmail.com>
2023-04-17 14:52:33 +0100

    Complete mocking_bites crafting_doubles

--- mocking_bites/02_crafting_doubles/exercise_spec.rb
+++ mocking_bites/02_crafting_doubles/exercise_spec.rb
@@ -1,27 +1,32 @@
 RSpec.describe "doubles" do
   # Change `xit` to `it` one by one as you work through.
 
-  xit "sets up a blank double" do
+  it "sets up a blank double" do
     # Set up your doubles here
-    fake_object = # ...
+    fake_object = double :object
+    
 
     # Don't edit below
     expect(fake_object).to be
   end
 
-  xit "sets up a double with methods" do
+  it "sets up a double with methods" do
     # Set up your doubles here
-    fake_object = # ...
-
+    fake_object = double :object
+    allow(fake_object).to receive(:speak).and_return("Meow")
+    allow(fake_object).to receive(:count_ears).and_return(2)
+    allow(fake_object).to receive(:count_legs).and_return(4)
+    
     # Don't edit below
     expect(fake_object.speak).to eq "Meow"
     expect(fake_object.count_ears).to eq 2
     expect(fake_object.count_legs).to eq 4
   end
 
-  xit "sets up a double with methods that take arguments" do
+  it "sets up a double with methods that take arguments" do
     # Set up your doubles here
-    fake_object = # ...
+    fake_object = double :fake_object
+    allow(fake_object).to receive(:speak).with("Jess").and_return("Meow, Jess")
 
     # Don't edit below
     expect(fake_object.speak("Jess")).to eq "Meow, Jess"
@@ -30,21 +35,21 @@ RSpec.describe "doubles" do
     expect { fake_object.speak("Ron") }.to raise_error RSpec::Mocks::MockExpectationError
   end
 
-  xit "sets up doubles that expect to be called" do
+  it "sets up doubles that expect to be called" do
     fake_object = double :animal
     # Write an expectation below that the method "speak" is called with
     # the argument "Steve"
 
-    # ...
+    allow(fake_object).to receive(:speak).with("Steve")
 
     # Don't edit below
     fake_object.speak("Steve")
   end
 
-  xit "creates a double for a specific case" do
+  it "creates a double for a specific case" do
     fake_diary = double :diary, add: nil
     # Set up this double to pass the tests below
-    # ...
+    allow(fake_diary).to receive(:count_entries).and_return(2)
 
     # Don't edit below
     fake_diary.add(double :diary_entry)
