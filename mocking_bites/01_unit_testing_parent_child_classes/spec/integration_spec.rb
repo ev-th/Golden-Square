9345e2354d0f2fe911360e946a3786fd856015d5
Evan Thomas <evanthomas0292@gmail.com>
2023-04-17 10:27:33 +0100

    Add spec files and create integration spec skeleton

---
+++ mocking_bites/01_unit_testing_parent_child_classes/spec/integration_spec.rb
@@ -0,0 +1,26 @@
+require 'music_library'
+require 'track'
+
+describe "MusicLibrary integration" do
+  context "when tracks are added" do
+    xit "has a list of tracks" do
+    end
+
+    describe "#search" do
+      context "when multiple tracks match a keyword" do
+        xit "returns an array of all tracks that match the keyword" do
+        end
+      end
+
+      context "when one track matches a keyword" do
+        xit "returns an array with that track" do
+        end
+      end
+
+      context "when no tracks match a keyword" do
+        xit "returns an empty array" do
+        end
+      end
+    end
+  end
+end
