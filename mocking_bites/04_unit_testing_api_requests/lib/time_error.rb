f993a08a8571862333b80afa645f6919d45e77f3
Evan Thomas <evanthomas0292@gmail.com>
2023-04-17 21:44:24 +0100

    Write unit test for TimeError class

--- mocking_bites/04_unit_testing_api_requests/lib/time_error.rb
+++ mocking_bites/04_unit_testing_api_requests/lib/time_error.rb
@@ -3,17 +3,22 @@ require 'json'
 require 'net/http'
 
 class TimeError
-  # Returns difference in seconds between server time
-  # and the time on this computer
-  def error
-    return get_server_time - Time.now
+  def initialize(requester=Net::HTTP)
+    @requester = requester
+  end
+
+  def error(time=Time)
+    return (get_server_time - time.now)
   end
 
   private
 
   def get_server_time
-    text_response = Net::HTTP.get(URI("https://worldtimeapi.org/api/ip"))
+    text_response = @requester.get(URI("https://worldtimeapi.org/api/ip"))
     json = JSON.parse(text_response)
     return DateTime.parse(json["utc_datetime"]).to_time
   end
 end
+
+te = TimeError.new
+p te.error
--- mocking_bites/04_unit_testing_api_requests/spec/time_error_spec.rb
+++ mocking_bites/04_unit_testing_api_requests/spec/time_error_spec.rb
@@ -1,5 +1,18 @@
 require 'time_error'
 
 RSpec.describe TimeError do
-  
+  it "returns the difference in seconds between server time and current time" do
+    fake_requester = double :requester
+    allow(fake_requester).to receive(:get)
+    .with(URI("https://worldtimeapi.org/api/ip"))
+    .and_return("{\"utc_datetime\":\"2023-04-17T20:35:50.495375+00:00\"}")
+
+    fake_time = double :Time
+    time_object = Time.new(2023, 4, 17, 21, 35, 0)
+    allow(fake_time).to receive(:now).and_return(time_object)
+
+    time_error = TimeError.new(requester=fake_requester)
+    result = time_error.error(time=fake_time)
+    expect(result).to eq 50.495375
+  end
 end
