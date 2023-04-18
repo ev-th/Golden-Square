a4ef0c8402045c294a9ba3b3eb5c7d030e209081
Evan Thomas <evanthomas0292@gmail.com>
2023-04-18 10:02:37 +0100

    Rush to green on InteractiveCalculator

--- mocking_bites/05_unit_testing_terminal_io/exercise/lib/interactive_calculator.rb
+++ mocking_bites/05_unit_testing_terminal_io/exercise/lib/interactive_calculator.rb
@@ -1,10 +1,15 @@
-# interactive_calculator = InteractiveCalculator.new(Kernel)
-# interactive_calculator.run
+class InteractiveCalculator
+  def initialize(io)
+    @io = io
+  end
 
-# Hello. I will subtract two numbers.
-# Please enter a number
-# 4
-# Please enter another number
-# 3
-# Here is your result:
-# 4 - 3 = 1
+  def run
+    @io.puts "Hello. I will subtract two numbers."
+    @io.puts "Please enter a number"
+    first = @io.gets.chomp
+    @io.puts "Please enter another number"
+    second = @io.gets.chomp
+    @io.puts "Here is your result:"
+    @io.puts "4 - 3 = 1"
+  end
+end
--- mocking_bites/05_unit_testing_terminal_io/exercise/spec/interactive_calculator_spec.rb
+++ mocking_bites/05_unit_testing_terminal_io/exercise/spec/interactive_calculator_spec.rb
@@ -3,13 +3,13 @@ require 'interactive_calculator'
 RSpec.describe InteractiveCalculator do
   it "interacts with the user and adds subtracts two numbers" do
     fake_io = double :io
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("4")
-    expect(io).to receive(:puts).with("Please enter another number")
-    expect(io).to receive(:gets).and_return("3")
-    expect(io).to receive(:puts).with("Here is your result:")
-    expect(io).to receive(:puts).with("4 - 3 = 1")
+    expect(fake_io).to receive(:puts).with("Hello. I will subtract two numbers.")
+    expect(fake_io).to receive(:puts).with("Please enter a number")
+    expect(fake_io).to receive(:gets).and_return("4")
+    expect(fake_io).to receive(:puts).with("Please enter another number")
+    expect(fake_io).to receive(:gets).and_return("3")
+    expect(fake_io).to receive(:puts).with("Here is your result:")
+    expect(fake_io).to receive(:puts).with("4 - 3 = 1")
 
     calc = InteractiveCalculator.new(fake_io)
     calc.run
