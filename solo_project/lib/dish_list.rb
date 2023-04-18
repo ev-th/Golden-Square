ba57a4aa2a9cbdb642777f656040eba141c1ef43
Evan Thomas <evanthomas0292@gmail.com>
2023-04-18 22:30:43 +0100

    Test drive the Takeaway Project DishList class

--- solo_project/lib/dish_list.rb
+++ solo_project/lib/dish_list.rb
@@ -1,22 +1,23 @@
 class DishList
   def initialize
-    # ...
+    @dishes = []
   end
 
-  def add(dish) #dish is an instance of Dish
-    # adds a dish to the list
+  def add(dish)
+    @dishes << dish
   end
 
-  def remove(dish) #dish is an instance of Dish
-    # removes a dish from the list
-  end
-
-  def display(availability=true) # toggle availability to display without availability
-    # prints the list of dishes
+  def remove(dish)
+    fail "This item is not in the list" unless @dishes.include?(dish)
+    @dishes.delete(dish)
   end
 
   def all_available?
-    # returns a boolean
-    # true if all available, false if any not available
+    fail "there are no dishes in the list" if @dishes.empty?
+    @dishes.all?(&:available?)
+  end
+
+  def list
+    @dishes
   end
 end
---
+++ solo_project/spec/dish_list_spec.rb
@@ -0,0 +1,97 @@
+require 'dish_list'
+
+RSpec.describe DishList do
+  context "initially" do
+    it "has an empty list" do
+      menu = DishList.new
+      expect(menu.list).to eq []
+    end
+
+    describe "#all_available?" do
+      it "fails" do
+        menu = DishList.new
+        expect {
+          menu.all_available?
+        }.to raise_error "there are no dishes in the list"
+      end
+    end
+  end
+
+  context "when dishes are added" do
+    it "has a list of those dishes" do
+      menu = DishList.new
+      fake_pasta = double :Dish
+      fake_pizza = double :Dish
+      menu.add(fake_pasta)
+      menu.add(fake_pizza)
+      expect(menu.list).to eq [fake_pasta, fake_pizza]
+    end
+
+    it "can remove those dishes" do
+      menu = DishList.new
+      fake_pasta = double :Dish
+      fake_pizza = double :Dish
+      fake_risotto = double :Dish
+      menu.add(fake_pasta)
+      menu.add(fake_pizza)
+      menu.add(fake_risotto)
+      menu.remove(fake_pizza)
+      expect(menu.list).to eq [fake_pasta, fake_risotto]
+    end
+  end
+  
+  describe "#remove" do
+    context "when attempting to remove dishes that are not present" do
+      it "fails" do
+        menu = DishList.new
+        fake_pasta = double :Dish
+        fake_risotto = double :Dish
+        menu.add(fake_pasta)
+        expect {
+          menu.remove(fake_risotto)
+        }.to raise_error "This item is not in the list"
+      end
+    end
+  end
+  
+  describe "#all_available?" do
+    context "when all dishes are available" do
+      it "returns true" do
+        menu = DishList.new
+        fake_pasta = double :Dish, available?: true
+        fake_pizza = double :Dish, available?: true
+        fake_risotto = double :Dish, available?: true
+        menu.add(fake_pasta)
+        menu.add(fake_pizza)
+        menu.add(fake_risotto)
+        expect(menu.all_available?).to eq true
+      end
+    end
+
+    context "when one dish is not available" do
+      it "returns false" do
+        menu = DishList.new
+        fake_pasta = double :Dish, available?: true
+        fake_pizza = double :Dish, available?: false
+        fake_risotto = double :Dish, available?: true
+        menu.add(fake_pasta)
+        menu.add(fake_pizza)
+        menu.add(fake_risotto)
+        expect(menu.all_available?).to eq false
+      end
+    end
+
+    context "when all dishes are unavailable" do
+      it "returns false" do
+        menu = DishList.new
+        fake_pasta = double :Dish, available?: false
+        fake_pizza = double :Dish, available?: false
+        fake_risotto = double :Dish, available?: false
+        menu.add(fake_pasta)
+        menu.add(fake_pizza)
+        menu.add(fake_risotto)
+        expect(menu.all_available?).to eq false
+      end
+    end
+  end
+end
