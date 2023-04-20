b7a3e551d334756fd7f39500c503fb77d474e62a
Evan Thomas <evanthomas0292@gmail.com>
2023-04-20 13:25:13 +0100

    Add unit tests for TextMessageSender

--- solo_project/spec/text_message_sender_spec.rb
+++ solo_project/spec/text_message_sender_spec.rb
@@ -2,17 +2,10 @@ require 'text_message_sender'
 
 RSpec.describe TextMessageSender do
   it "sends a message" do
-    choices = DishList.new
-    pizza = Dish.new('pizza', 1199)
-    choices.add(pizza)
-    order = Order.new(choices)
-
-    fake_time = double :Time, now: Time.new(2023, 2, 2, 19, 30, 0)
-
-    expected_delivery_time = Time.new(2023, 2, 2, 20, 30, 0)
-    order.confirm!(fake_time)
-
+    fake_order = double :order, complete?: true, order_time: Time.new(2023, 2, 2, 19, 30, 0)
     
+    expected_delivery_time = Time.new(2023, 2, 2, 20, 30, 0)
+
 
     fake_json_response = '{"body": "Hi there", "direction": "outbound-api", "error_code": null, "error_message": null, "from": "+15017122661", "num_media": "0", "num_segments": "1", "price": null, "price_unit": null, "status": "sent", "to": "+15558675310",}'
 
@@ -30,9 +23,19 @@ RSpec.describe TextMessageSender do
       ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     ).and_return(fake_client)
 
-    messager = TextMessageSender.new(order, text_message_client=fake_api)
+    messager = TextMessageSender.new(fake_order, text_message_client=fake_api)
     response = messager.send
 
     expect(messager.send).to eq fake_json_response
+  end
+
+  context "when order is not complete" do
+    it "fails to send the message" do
+      fake_order = double :fake_order, complete?: false
+      text_message_sender = TextMessageSender.new(fake_order)
+      expect { text_message_sender.send }.to raise_error (
+        "The order must be confirmed before the text message can be sent"
+      )
+    end
   end
 end
